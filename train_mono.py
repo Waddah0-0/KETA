9619e83f0aad1d0dbf892841dc7a96819316c661
Waddah Ali <saumon6@gmail.com>
2026-07-01 04:05:01 +0300

    Revert SFTConfig parameters to match older TRL 0.24

--- train_mono.py
+++ train_mono.py
@@ -97,8 +97,6 @@ def main():
         processing_class=tokenizer,
         train_dataset=dataset,
         args=SFTConfig(
-            max_seq_length=args.max_seq_length,
-            eos_token="<|im_end|>",
             packing=False,
             per_device_train_batch_size=args.batch_size,
             gradient_accumulation_steps=args.grad_accum_steps,
